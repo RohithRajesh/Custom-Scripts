eb00e48d79cbc1037093503fce0110b481aa40f5
RohithRajesh <rohith18182@iiitd.ac.in>
2021-03-29 15:12:44 +0530

    Alert stop when charging but percentage below 20

--- battery_control.sh
+++ battery_control.sh
@@ -2,9 +2,9 @@
 
 BATFILE=`/usr/bin/acpi | /usr/bin/grep 'Battery 0' | /usr/bin/awk '{print int($4)}'`
 export DISPLAY=:0
+STAT=`acpi | grep "Battery 0" | awk '{print $3}'` 
 
-
-if [ $BATFILE -le 20 ] 
+if [[  $BATFILE -le 20 ]]  && [[ $STAT = "Discharging," ]]
 then
   sudo -u rohith DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus notify-send -u critical "Battery Low: ""$BATFILE""%"
 fi
